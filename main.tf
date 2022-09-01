7ca4e4982ab631e3b1f862ce0970fc05a240b91f
palskim <kps0506@gmail.com>
2022-09-01 16:51:15 +0900

    third commit

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ resource "aws_instance" "example" {
     user_data = <<-EOF
                 #!/bin/bash
                 echo "Hello, World" > index.html
-                nohup busybox httpd -f -p 8080 &
+                nohup busybox httpd -f -p ${var.server_port} &
                 EOF
 
     tags = {
@@ -22,12 +22,23 @@ resource "aws_security_group" "instance" {
     name = "terraform-example-instance"
 
     ingress {
-        from_port   = 8080
-        to_port     = 8080
+        from_port   = var.server_port
+        to_port     = var.server_port
         protocol    = "tcp"
         cidr_blocks = ["203.246.171.161/32"]
     }
     
 }
 
-## 테스트 (secondary commit)
+variable "server_port" {
+  type        = number
+  default     = 8080
+  description = "The port the server will use for HTTP requests"
+}
+
+output "public_ip" {
+  value       = aws_instance.example.public_ip
+  #sensitive   = true
+  description = "The public ip address of the web server"
+  #depends_on  = []
+}
